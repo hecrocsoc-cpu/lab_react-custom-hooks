f41208b922f91e77d3eaf624f0f3e26e8d7fb61e
Héctor Rguez <hecrocsoc@gmail.com>
2026-04-16 18:52:34 +0100

    solved

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import PublicGists from './components/PublicGists';
+import UserGists from './components/UserGists';
+
+function App() {
+  return (
+    <div>
+      <h1>React Custom Hooks Lab</h1>
+      <PublicGists />
+      <hr />
+      <UserGists />
+    </div>
+  );
+}
+
+export default App;
